29bff80015f14023fc00f5edd11ac7c27627904e
Anishd <anishd@ip-192-168-1-7.ap-south-1.compute.internal>
2022-01-13 19:44:29 +0530

    removed console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
 
-console.log('client side js is loaded')
+//console.log('client side js is loaded')
 
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
