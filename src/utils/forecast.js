0125a418c222ba52ddd6b9d1af5fd5214962702c
anish513 <anish.d1947@gmail.com>
2022-01-14 10:22:52 +0530

    added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,18 +1,16 @@
-const request=require('request')
-const forecast=(latitude,longitude,callback)=>{
-    const url='http://api.weatherstack.com/current?access_key=46d48002afd65fe78b9f448585e49b1a&query='+latitude+','+longitude+'&units=f'
-    request({url,json:true},(error,{body})=>{
-        if(error){
-            callback("unable to connect to weather serice",undefined);
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=46d48002afd65fe78b9f448585e49b1a&query=' + latitude + ',' + longitude + '&units=f'
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("unable to connect to weather serice", undefined);
         }
-        else if(body.error)
-            {
+        else if (body.error) {
             callback("unable to find the given location");
-            }
-        else{
-            
-            callback(undefined,body.current.weather_descriptions[0]+" it currently  "+body.current.temperature+" it feels like "+body.current.feelslike)
+        }
+        else {
+            callback(undefined, body.current.weather_descriptions[0] + " it currently  " + body.current.temperature + " it feels like " + body.current.feelslike + ".The Humidity is  " + body.current.humidity + " % ");
         }
     });
 }
-module.exports=forecast;
+module.exports = forecast;
